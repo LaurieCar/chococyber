9db3724c832fe75a0e3a8f3ee3051de55f4b70ab
LaurieCar <laurie.carriere26@gmail.com>
2024-01-04 19:38:06 +0100

    suite addUser

--- app/controller/ctrl_utilisateur.php
+++ app/controller/ctrl_utilisateur.php
@@ -3,6 +3,43 @@
     function insertUtilisateur($bdd){
         // stocker les messages d'erreur
         $message = "";
+        // test si le bouton est cliqué
+        if(isset($_POST["submit"])){
+            // test si les champs sont remplis
+            if(!empty($_POST["nom_utilisateur"]) AND !empty($_POST["prenom_utilisateur"]) AND !empty($_POST["mail_utilisateur"]) AND !empty($_POST["password_utilisateur"]) AND !empty($_POST["password_verif"])){
+                // test si les 2 password sont identiques
+                if($_POST["password_utilisateur"]===$_POST["password_verif"]){
+                    // nettoyer mail et password
+                    $email = cleanInput($_POST["mail_utilisateur"]);
+                    $password = cleanInput($_POST["password_utilisateur"]);
+                    $password_verif = cleanInput($_POST["password_verif"]);
+                    // nettoyer nom et prenom
+                    $nom = cleanInput($_POST["nom_utilisateur"]);
+                    $prenom = cleanInput($_POST["prenom_utilisateur"]);
+                    // hasher le mdp
+                    $hash = password_hash($password,PASSWORD_DEFAULT);
+                    $hash_verif = password_hash($password_verif,PASSWORD_DEFAULT);
+                    // test si l'utilisateur n'existe pas
+                    if(empty(getUtilisateurByMail($bdd,$email))){
+                        // ajouter l'utilisateur en bdd
+                        addUtilisateur($bdd,$nom,$prenom,$email,$hash,$hash_verif);
+                        $message = "l'utilisateur ".$nom .$prenom." a été ajouté en bdd";
+                    }
+                    // test si l'utilisateur existe en bdd
+                    else{
+                        $message = "l'utilisateur ".$nom .$prenom." existe deja en bdd";
+                    }
+                }
+                // test si les mots de passe sont différents
+                else{
+                    $message = "Les mots de passe ne sont pas identiques";
+                }
+            }
+            // test si les champs ne sont pas remplis
+            else{
+                $message = "Veuillez remplir tous les champs du formulaire";
+            }
+        }
         
         // importer la vue (fichier html)
         include_once './app/vue/vue_add_utilisateur.php';
--- app/model/model_utilisateur.php
+++ app/model/model_utilisateur.php
@@ -12,17 +12,20 @@
         }
     }
 
-    function addUtilisateur($bdd,$nom,$prenom,$email,$password,$image){
+    // methode pour ajouter un utilisateur en bdd
+    function addUtilisateur($bdd,$nom,$prenom,$email,$password,$image,$id_roles){
         try {
-            $requete = $bdd->prepare('INSERT INTO utilisateur(nom_utilisateur,prenom_utilisateur,mail_utilisateur,password_utilisateur,image_utilisateur) VALUE(?,?,?,?,?)');
+            $requete = $bdd->prepare('INSERT INTO utilisateur(nom_utilisateur,prenom_utilisateur,mail_utilisateur,password_utilisateur,image_utilisateur,id_roles) VALUE(?,?,?,?,?,?)');
             $requete->bindParam(1,$nom,PDO::PARAM_STR);
             $requete->bindParam(2,$prenom,PDO::PARAM_STR);
             $requete->bindParam(3,$email,PDO::PARAM_STR);
             $requete->bindParam(4,$password,PDO::PARAM_STR);
             $requete->bindParam(5,$image,PDO::PARAM_STR);
-            re
-        } catch (\Throwable $th) {
-            //throw $th;
+            $requete->bindParam(6,$id_roles,PDO::PARAM_STR);
+            $requete->execute();
+        } 
+        catch (\Exception $e){
+            die(''.$e->getMessage());
         }
     }
 
